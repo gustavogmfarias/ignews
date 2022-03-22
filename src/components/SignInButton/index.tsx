839bbfa58546c319bcab3f36900c92114d876317
GUSTAVO GOULART DE MELLO FARIAS <53410368+gustavogmfarias@users.noreply.github.com>
2022-03-22 11:29:42 -0300

    erro console session

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -5,8 +5,8 @@ import { signIn, useSession } from "next-auth/react";
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const [session] = useSession();
-
+  const { data: session } = useSession();
+  console.log(session);
   return session ? (
     <button
       type="button"
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,12 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
-export default NextAuth({
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      authorization: {
-        params: {
-          scope: "read:user",
-        },
-      },
-    }),
-  ],
+GithubProvider({
+  clientId: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  authorization: {
+    params: {
+      scope: "read:user",
+    },
+  },
 });
