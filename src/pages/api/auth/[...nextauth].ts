248844b36a6983b62042d8dadd08e0d4a6de5825
GUSTAVO GOULART DE MELLO FARIAS <53410368+gustavogmfarias@users.noreply.github.com>
2022-04-06 19:37:48 -0300

    Não consigo buscar o userActiveSubscription

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,4 @@
 import { query } from "faunadb";
-
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { fauna } from "../../../services/fauna";
@@ -18,6 +17,37 @@ export default NextAuth({
   ],
 
   callbacks: {
+    async session({ session }) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          query.Get(
+            query.Intersection([
+              query.Match(
+                query.Index("subscription_by_user_ref"),
+                query.Select(
+                  "ref",
+                  query.Get(
+                    query.Match(
+                      query.Index("user_by_email"),
+                      query.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+
+              query.Match(query.Index("subscription_by_status"), "active"),
+            ])
+          )
+        );
+        return { ...session, activeSubscription: userActiveSubscription };
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        };
+      }
+    },
+
     async signIn({ user, account, profile }) {
       const { email } = user;
 
--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -18,7 +18,11 @@ export const config = {
   },
 };
 
-const relevantEvents = new Set(["checkout.session.completed"]);
+const relevantEvents = new Set([
+  "checkout.session.completed",
+  "customer.subscription.updated",
+  "customer.subscription.deleted",
+]);
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (request: NextApiRequest, response: NextApiResponse) => {
@@ -43,12 +47,23 @@ export default async (request: NextApiRequest, response: NextApiResponse) => {
     if (relevantEvents.has(type)) {
       try {
         switch (type) {
+          case "customer.subscription.updated":
+          case "customer.subscription.deleted":
+            const subscription = event.data.object as Stripe.Subscription;
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            );
+            break;
+
           case "checkout.session.completed":
             const checkoutSession = event.data
               .object as Stripe.Checkout.Session;
             await saveSubscription(
               checkoutSession.subscription.toString(),
-              checkoutSession.customer.toString()
+              checkoutSession.customer.toString(),
+              true
             );
             break;
           default:
